dd1a5fe8ee36c28d6ca9e0e893d37fda76935524
Sebastian Edwards <me@sebastianedwards.co.nz>
2015-01-16 00:16:52 +1300

    Remove a superfluous transport test.

--- spec/lib/aggro/nanomsg_transport_spec.rb
+++ spec/lib/aggro/nanomsg_transport_spec.rb
@@ -7,21 +7,10 @@ RSpec.describe NanomsgTransport do
     client = NanomsgTransport.client host
 
     client.post message
+
     server.stop
     client.close_socket
 
     expect(@rec).to eq message
-  end
-
-  it 'should allow multiple clients' do
-    count = 50
-    server = NanomsgTransport.server(host) { |req| req }.start
-    clients = count.times.map { NanomsgTransport.client(host) }
-
-    res = clients.each_with_index { |client, i| client.post i.to_s }
-    server.stop
-    clients.map(&:close_socket)
-
-    expect(res.length).to eq 50
   end
 end
