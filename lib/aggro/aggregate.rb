470c6238c1e9318bdf230c82b510e97a5902623b
Sebastian Edwards <me@sebastianedwards.co.nz>
2015-01-17 22:17:50 +1300

    Add command handler registration to Aggregate.

--- lib/aggro/aggregate.rb
+++ lib/aggro/aggregate.rb
@@ -5,8 +5,22 @@ module Aggro
 
     # Public: Adds class interface to aggregate.
     module ClassMethods
+      def allows(command_class, &block)
+        command_handlers[command_class] = block if block
+      end
+
+      def allows?(command_class)
+        command_handlers.keys.include? command_class
+      end
+
       def find(id)
         AggregateRef.new id, name
+      end
+
+      private
+
+      def command_handlers
+        @command_handlers ||= {}
       end
     end
   end
--- spec/lib/aggro/aggregate_spec.rb
+++ spec/lib/aggro/aggregate_spec.rb
@@ -1,9 +1,38 @@
 RSpec.describe Aggregate do
-  class Cat
-    include Aggregate
+  class TestCommand
   end
 
-  describe '#find' do
+  class Cat
+    include Aggregate
+
+    allows TestCommand do |command|
+      command.thing
+    end
+  end
+
+  describe '.allows' do
+    it 'should register a command handler' do
+      Cat.allows(Fixnum) { true }
+
+      expect(Cat.allows?(Fixnum)).to be_truthy
+    end
+  end
+
+  describe '.allows?' do
+    context 'input command has a registeded handler' do
+      it 'should return true' do
+        expect(Cat.allows?(TestCommand)).to be_truthy
+      end
+    end
+
+    context 'input command does not have a registed handler' do
+      it 'should return false' do
+        expect(Cat.allows?(String)).to be_falsey
+      end
+    end
+  end
+
+  describe '.find' do
     let(:aggregate_ref) { double }
     let(:aggregate_ref_class) { spy(new: aggregate_ref) }
 
