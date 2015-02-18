fcd50e461ed6ed898c8f516b1227e819d809f728
Sebastian Edwards <me@sebastianedwards.co.nz>
2015-02-19 12:22:17 +1300

    Loosen valid boolean transform inputs.

--- lib/aggro/transform/boolean.rb
+++ lib/aggro/transform/boolean.rb
@@ -5,12 +5,24 @@ module Aggro
       module_function
 
       def deserialize(value)
-        value if value == false || value == true
+        value if truthy?(value) || falsey?(value)
       end
 
       def serialize(value)
-        value if value == false || value == true
+        value if truthy?(value) || falsey?(value)
       end
+
+      def falsey?(value)
+        value == false || value == 'false' || value == '0'
+      end
+
+      private :falsey?
+
+      def truthy?(value)
+        value == true || value == 'true' || value == '1'
+      end
+
+      private :truthy?
     end
   end
 end
